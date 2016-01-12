6570929a7fa718413a91c2138170873c759a49a9
pattyboyo <patrick2hb@hotmail.com>
2016-01-11 21:15:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-$(document).ready(function() {
-    $('button').click(function() {
-    	var toAdd = $("input[name=do it]").val();
-        $('#messages').append("<p>"+toAdd+"</p>");
-    });
-});
+
