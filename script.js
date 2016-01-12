92859f17269294b5da9ab1e3959029ca5a45a757
pattyboyo <patrick2hb@hotmail.com>
2016-01-11 21:02:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
       var theMessage = $("input[name=write whatever u want!]").val();
       $('button').click(function() {
-            $('#message').append("<p>"+theMessage+"</p>")
+            $('#messages').append("<p>"+theMessage+"</p>")
       }
       
       )
