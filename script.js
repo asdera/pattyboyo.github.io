2854987705d0e19e57c0bb9ef92dfe1aee6a2e1b
pattyboyo <patrick2hb@hotmail.com>
2016-01-11 20:39:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-      $('#accordion').accordion();
+      var theMessage = $("input[name=write whatever you want!]").val();
+      $('button').click(function() {
+            $('#message').append('<p>theMessage</p>')
+      }
+      
+      )
       });
