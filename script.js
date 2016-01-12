13a6f5866ba56f2da3f6a06baa4173cee0156d16
pattyboyo <patrick2hb@hotmail.com>
2016-01-11 20:47:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-      var theMessage = $("input[name=write whatever you want!]").val();
+      var theMessage = $("input[name='write whatever u want!']").val();
       $('#button').click(function() {
-            $('#message').append('<p class="message">theMessage</p>')
+            $('#message').append("<p class="message">"+theMessage+"</p>")
       }
       
       )
