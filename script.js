1245f3ea960254ff41b9d0f2094440d1667ae234
pattyboyo <patrick2hb@hotmail.com>
2016-01-12 20:34:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
     background-color:"green";
   } );
 });
+  $(document).keyDown(function(key) {
+    switch(parseInt(key.which,10)) {
+      case 37:
+        $('img').animate({Left:'-=10'},300)
+        break;
+        case 39:
+        $('img').animate({Left:'+=10'},300)
+        break;
+    }
+  }
 })
