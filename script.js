e3cab6143427ea9e9767aa5a33ab98d2c146d70b
pattyboyo <patrick2hb@hotmail.com>
2016-01-13 16:58:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $( "#explode").click(function() {
   $( "#explode" ).animate( {
-    background-color:"green";
+    background-color:"rgb: 20, 20, 20";
   } );
 
  
