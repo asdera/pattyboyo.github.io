d305e51b39665683166f4712469633bde074db46
pattyboyo <patrick2hb@hotmail.com>
2016-01-25 18:19:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   $(document).ready(function(){
     $("button").click(function(){
         $(".sidebar").animate({
-            left: 10px;
+            background-color: 'ffff99';
         });
     });
 });
