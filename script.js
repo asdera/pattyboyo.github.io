4bd529c96c512eb5236cc80fa9e39e1203543316
pattyboyo <patrick2hb@hotmail.com>
2016-01-25 18:37:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
   $(document).ready(function(){
     $("button").click(function(){
-        $('.class').append("<p>secret stuff</p>, <p>more secret stuff</p>")
+        $('.class').append("<p>secret stuff</p>", , "<p>more secret stuff</p>")
             
         
     });
