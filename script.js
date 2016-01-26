b5d06bae54c850c68a84ac3cf5ffc521d16b43e2
pattyboyo <patrick2hb@hotmail.com>
2016-01-25 19:10:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   $(document).ready(function(){
     $("button").click(function(){
         $('.class').append("<p>secret stuff</p>")
-        $('body').append("<div class='supersecret'></div>")
+        
             
         
     });
