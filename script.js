ecab45625ee02f54e4068de5d6f8c4baf8ebd715
pattyboyo <patrick2hb@hotmail.com>
2016-02-09 19:16:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
 
-  $(document).ready(function(){
-    $("button").click(function(){
-        $('.class').append("<p>secret stuff</p>")
-        
-            
-        
-    });
-});
+var myName = "patrick";
+letterColors = [red, orange, green, blue, purple]
+if(10 > 3) {
+    bubbleShape = "circle";
+}
+
+else {
+    bubbleShape = "square";
+}
+drawName(myName, letterColors);
+bounceBubbles()
